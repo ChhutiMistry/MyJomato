11c3461608fae0f43100f3fbe0bb93ce1214b4e9
chhuti.mistry <chhuti246@gmail.com>
2022-03-01 16:10:30 +0530

    twelfth change

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function changemode(){
     var mybody = document.body;
     mybody.classList.toggle(`mydark`)
 }
+
+function loadcoupon(){
+    document.getElementById('coupon').style.display = 'block';
+}
+
+function closecoupon(){
+    document.getElementById('coupon').style.display = 'none'
+}
