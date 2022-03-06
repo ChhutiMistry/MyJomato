20d4bd9c79ae87a88c6d3c9bbf3f01350f0f5d70
chhuti.mistry <chhuti246@gmail.com>
2022-03-06 21:24:36 +0530

    fourteenth change

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ function loadcoupon(){
 function closecoupon(){
     document.getElementById('coupon').style.display = 'none';
 }
+
+let Geolocation=()=>{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("GeoLocation not supported.")
+    }
+}
+
+let showPosition=(data)=>{
+    let x = document.getElementById('weather');
+    let lon=data.coords.longitude;
+    let lat=data.coords.latitude;
+    console.log(data);
+    let url=`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+   
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>{
+        let cityName = data.city.name
+        console.log(data);
+        data.list.map((item)=>{
+            console.log(item.temp.day)
+            x.innerText=`Current temperature of ${cityName} is ${item.temp.day} degree Celsius`;
+        })
+    })
+}
+Geolocation();
