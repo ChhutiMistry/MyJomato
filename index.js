df6f5cbeb0421f6dd30442ac9b9fdef1864aa37c
chhuti.mistry <chhuti246@gmail.com>
2022-03-07 07:48:34 +0530

    fourteenth change

--- index.js
+++ index.js
@@ -13,29 +13,29 @@ function closecoupon(){
     document.getElementById('coupon').style.display = 'none';
 }
 
-let Geolocation=()=>{
+let Geolocation = () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        console.log("GeoLocation not supported.")
+        console.log("Your Location is not supported.")
     }
 }
 
-let showPosition=(data)=>{
-    let x = document.getElementById('weather');
-    let lon=data.coords.longitude;
-    let lat=data.coords.latitude;
+let showPosition=(data) => {
+    let x = document.getElementById('myweather');
+    let lon = data.coords.longitude;
+    let lat = data.coords.latitude;
     console.log(data);
-    let url=`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    let url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
    
     fetch(url)
-    .then((res)=>res.json())
-    .then((data)=>{
+    .then((res) => res.json())
+    .then((data) => {
         let cityName = data.city.name
         console.log(data);
-        data.list.map((item)=>{
+        data.list.map((item) => {
             console.log(item.temp.day)
-            x.innerText=`Current temperature of ${cityName} is ${item.temp.day} degree Celsius`;
+            x.innerText = `Average temperature of ${cityName} is ${item.temp.day} degree Celsius`;
         })
     })
 }
